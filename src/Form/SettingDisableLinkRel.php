87f6c7ec2f33203225f573f5ed8a97cdd3dfbf29
batkor <info@batkor.ru>
2017-09-21 16:49:36 +0800

    add remove attr to link

--- src/Form/SettingDisableLinkRel.php
+++ src/Form/SettingDisableLinkRel.php
@@ -47,6 +47,23 @@ class SettingDisableLinkRel extends ConfigFormBase {
       '#description' => 'Введите атрибуты через запятую. Например: <i>canonical, shortlink, delete-form</i>'
     ];
 
+    $form['remove_link_attr'] = [
+      '#title'         => 'Удалить атрибуты из ссылок',
+      '#type'          => 'checkbox',
+      '#default_value' => $config->get('remove_link_attr') ? $config->get('remove_link_attr') : NULL,
+    ];
+
+    $form['remove_link_attr_list'] = [
+      '#title'         => 'Введите атрибуты для удаления',
+      '#type'          => 'textfield',
+      '#default_value' => $config->get('remove_link_attr_list') ? $config->get('remove_link_attr_list') : '',
+      '#description' => 'Введите атрибуты через запятую. Например: <i>data-drupal-link-system-path, system-path</i>'
+    ];
+    $form['cach_clear'] = [
+      '#title'         => 'Очистить кэш при сохранении настроек',
+      '#type'          => 'checkbox',
+      '#default_value' => TRUE,
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -55,6 +72,11 @@ class SettingDisableLinkRel extends ConfigFormBase {
     $config = $this->config('disable_link_rel.import');
     $config->set('enable', $form_state->getValue('enable'));
     $config->set('links', $form_state->getValue('links'));
+    $config->set('remove_link_attr', $form_state->getValue('remove_link_attr'));
+    $config->set('remove_link_attr_list', $form_state->getValue('remove_link_attr_list'));
     $config->save();
+    if($form_state->getValue('cach_clear', FALSE)){
+      drupal_flush_all_caches();
+    }
   }
 }
